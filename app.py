17a84113138bcde00743abef5c61e2c7d354cf1a
Kurt Harriger <kurtharriger@gmail.com>
2023-10-22 18:47:10 -0600

    Replace hello with greeting api

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, jsonify
+from pydantic import BaseModel
+from flask_openapi3 import Info, Tag
+from flask_openapi3 import OpenAPI
 
-app = Flask(__name__)
+info = Info(title="Greeting API", version="1.0")
+app = OpenAPI(__name__, info=info)
+
+greeting_tag = Tag(name='greeting', description='Friendly Greeting')
+
+class GreetingQuery(BaseModel):
+    name: str
+
+class Greeting(BaseModel):
+    message: str
+
+@app.get('/greeting', tags=[greeting_tag])
+def get_greeting(query: GreetingQuery):
+      greeting = Greeting(message=f"Hello {query.name}!")
+      return jsonify(greeting.dict())
 
 @app.route('/')
 def hello_world():
     """Return a friendly greeting."""
-    return "Hello TechStars!"
+    return redirect('/openapi')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
